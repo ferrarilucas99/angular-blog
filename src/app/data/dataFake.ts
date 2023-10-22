16615798a1cb364b021c9dcde3179f6cf55d7bfc
Lucas Ferrari <lucashols65@gmail.com>
2023-10-22 14:38:24 -0300

    modificando o blog para noticias de economia

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,30 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "Governo cogita usar 'cashback' para reduzir impacto da reforma tributária na conta de luz de famílias baixa renda",
+    "description": "Estados têm isenção ou redução de tributos para essa população. Texto da reforma deve unificar alíquotas estaduais e eliminar exceções.",
+    "photoCover":"https://s2-g1.glbimg.com/Frt_P5BlOPgLYINRNmu9AtPRXdI=/0x0:1500x1000/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/h/p/MmJ6uhQYmzAAu3GS2nOA/gerente-homem-verificando-financas-de-trabalho.jpg",
+    "author": "G1",
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "Após venda de fábrica, Justiça de SP manda leiloar por R$ 27,5 milhões marca Chocolates Pan para pagar credores",
+    "description": "Na análise da perícia judicial, a marca de chocolates tem o faturamento estimado em R$ 51 milhões em cinco anos. Fábrica em São Caetano do Sul, na Grande SP, foi arrematada pela Cacau Show por R$ 71 milhões.",
+    "photoCover":"https://s2-g1.glbimg.com/I3L7q-2aUfcqGbtP-311QXNnkq0=/0x0:1055x595/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/R/N/EP292rRVaZiiM7AA4Z4g/1697832650658558.jpg",
+    "author": "G1",
+  },
+  {
+    "id":"3",
+    "title": "Dólar fecha em queda e volta a R$ 5,03, com juros dos EUA e guerra no Oriente Médio no radar; Ibovespa cai",
+    "description": "A moeda norte-americana teve um recuo de 0,45%, cotada a R$ 5,0302. Já o Ibovespa, principal índice acionário da bolsa de valores brasileira, a B3, caiu 0,74%, aos 113.155 pontos.",
+    "photoCover":"https://s2-g1.glbimg.com/hcA8EBQ_9pKi2c8lzROU6CRXUVE=/0x0:5472x3648/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/2/b/nYiAaBS6OqSUd2kK4Mrg/pexels-burak-the-weekender-186461.jpg",
+    "author": "G1",
+  },
+  {
+    "id":"4",
+    "title": "Confira as vagas de emprego disponíveis em Petrolina, Araripina e Salgueiro nesta sexta-feira (20)",
+    "description": "As vagas são disponibilizadas diariamente pela Agência do Trabalho de Pernambuco.",
+    "photoCover":"https://s2-g1.glbimg.com/8v5gXkMld1ExuTB2qL4WnGWQCJU=/0x0:984x739/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2022/L/t/0D18A1Sl6umMBnAxH0HQ/whatsapp-image-2022-08-03-at-15.01.46.jpeg",
+    "author": "G1",
   }
 ]
